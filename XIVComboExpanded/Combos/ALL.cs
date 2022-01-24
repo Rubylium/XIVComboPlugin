62f013603b56bb92626b4a992e29f8b6bf4bef7d
Rubylium <alexisoko0@gmail.com>
2022-01-24 22:47:47 +0100

    Create ALL.cs

---
+++ XIVComboExpanded/Combos/ALL.cs
@@ -0,0 +1,31 @@
+﻿namespace XIVComboExpandedPlugin.Combos
+{
+    internal static class ALL
+    {
+        public const byte ClassID = 0;
+        public const byte JobID = 0;
+
+        public const uint
+            HeadGraze = 7551,
+            SecondWind = 7541;
+
+        public static class Buffs
+        {
+            public const ushort
+                Placeholder = 0;
+        }
+
+        public static class Debuffs
+        {
+            public const ushort
+                Placeholder = 0;
+        }
+
+        public static class Levels
+        {
+            public const byte
+                HeadGraze = 24,
+                SecondWind = 8;
+        }
+    }
+}

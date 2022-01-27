4adc102b293bd95fe2d5c07af2d38a6e9961598d
Rubylium <alexisoko0@gmail.com>
2022-01-27 15:06:51 +0100

    Update WHM.cs

--- XIVComboExpanded/Combos/WHM.cs
+++ XIVComboExpanded/Combos/WHM.cs
@@ -10,6 +10,12 @@ namespace XIVComboExpandedPlugin.Combos
 
         public const uint
             Cure = 120,
+            Stone = 119,
+            Glare = 16533,
+            Glare3 = 25859,
+            Dia = 16532,
+            Aero = 121,
+            Aero2 = 132,
             Medica = 124,
             Cure2 = 135,
             PresenceOfMind = 136,
@@ -24,28 +30,119 @@ namespace XIVComboExpandedPlugin.Combos
             AfflatusMisery = 16535,
             Temperance = 16536,
             Holy3 = 25860,
+            Regen = 137,
             Aquaveil = 25861,
             LiturgyOfTheBell = 25862;
 
         public static class Buffs
         {
             public const ushort
-                Placeholder = 0;
+                Freecure = 155;
         }
 
         public static class Debuffs
         {
             public const ushort
-                Placeholder = 0;
+                Aero = 143,
+                Aero2 = 144,
+                Aero3 = 798,
+                Dia = 1871;
         }
 
         public static class Levels
         {
             public const byte
                 Cure2 = 30,
+                Stone = 1,
+                Glare = 72,
+                Glare3 = 82,
+                Aero = 4,
+                Aero2 = 46,
+                Dia = 72,
+                Regen = 35,
+                Benediction = 50,
+                Tetragrammaton = 60,
                 AfflatusSolace = 52,
                 AfflatusMisery = 74,
                 AfflatusRapture = 76;
+        }
+    }
+
+
+    internal class WhiteMageCure2 : CustomCombo
+    {
+        protected internal override CustomComboPreset Preset { get; } = CustomComboPreset.WhmAny;
+
+        protected override uint Invoke(uint actionID, uint lastComboMove, float comboTime, byte level)
+        {
+            if (actionID == WHM.Cure)
+            {
+                var gauge = GetJobGauge<WHMGauge>();
+
+                if (level >= ALL.Levels.LucidDream && GetPlayerMana() <= 40 && IsOffCooldown(ALL.LucidDream))
+                    return ALL.LucidDream;
+
+                if (level >= WHM.Levels.AfflatusSolace && gauge.Lily > 0 && GetTargetHealth() <= 60)
+                    return WHM.AfflatusSolace;
+
+                if (level >= WHM.Levels.Regen && GetTargetHealth() >= 90 && IsOffCooldown(WHM.Regen))
+                    return OriginalHook(WHM.Regen);
+
+                if (level >= WHM.Levels.Tetragrammaton && GetTargetHealth() <= 35 && IsOffCooldown(WHM.Tetragrammaton))
+                    return WHM.Tetragrammaton;
+
+                if (level >= WHM.Levels.Benediction && GetTargetHealth() <= 35 && GetTargetHealth() > 0 &&
+                    IsOffCooldown(WHM.Benediction))
+                    return WHM.Benediction;
+
+                if (level >= WHM.Levels.Cure2 && HasEffect(WHM.Buffs.Freecure))
+                    return WHM.Cure2;
+            }
+
+            if (actionID == WHM.Stone)
+            {
+                var gauge = GetJobGauge<WHMGauge>();
+                if (level >= ALL.Levels.LucidDream && GetPlayerMana() <= 40 && IsOffCooldown(ALL.LucidDream))
+                    return ALL.LucidDream;
+
+                if (level >= WHM.Levels.AfflatusMisery && gauge.BloodLily == 3)
+                    return WHM.AfflatusMisery;
+
+                if (level >= WHM.Levels.Dia)
+                {
+                    if (!TargetHasEffect(WHM.Debuffs.Dia))
+                    {
+                        return OriginalHook(WHM.Dia);
+                    }
+
+                    if (FindTargetEffect(WHM.Debuffs.Dia).RemainingTime <= 10)
+                    {
+                        return OriginalHook(WHM.Dia);
+                    }
+                }
+
+                if (level < WHM.Levels.Dia)
+                {
+                    if (level >= WHM.Levels.Aero2 && !TargetHasEffect(WHM.Debuffs.Aero2))
+                        return OriginalHook(WHM.Aero);
+
+                    if (level < WHM.Levels.Aero2 && level >= WHM.Levels.Aero && !TargetHasEffect(WHM.Debuffs.Aero))
+                        return OriginalHook(WHM.Aero);
+
+
+                    if (level >= WHM.Levels.Aero2 && FindTargetEffect(WHM.Debuffs.Aero).RemainingTime <= 10)
+                        return OriginalHook(WHM.Aero);
+
+                    if (level < WHM.Levels.Aero2 && level >= WHM.Levels.Aero &&
+                        FindTargetEffect(WHM.Debuffs.Aero2).RemainingTime <= 10)
+                        return OriginalHook(WHM.Aero);
+                }
+
+
+                return OriginalHook(WHM.Stone);
+            }
+
+            return actionID;
         }
     }
 
@@ -103,38 +200,6 @@ namespace XIVComboExpandedPlugin.Combos
         }
     }
 
-    internal class WhiteMageCure2 : CustomCombo
-    {
-        protected internal override CustomComboPreset Preset { get; } = CustomComboPreset.WhmAny;
-
-        protected override uint Invoke(uint actionID, uint lastComboMove, float comboTime, byte level)
-        {
-            if (actionID == WHM.Cure2)
-            {
-                var gauge = GetJobGauge<WHMGauge>();
-
-                if (IsEnabled(CustomComboPreset.WhiteMageCureFeature))
-                {
-                    if (level < WHM.Levels.Cure2)
-                        return WHM.Cure;
-                }
-
-                if (IsEnabled(CustomComboPreset.WhiteMageSolaceMiseryFeature))
-                {
-                    if (level >= WHM.Levels.AfflatusMisery && gauge.BloodLily == 3)
-                        return WHM.AfflatusMisery;
-                }
-
-                if (IsEnabled(CustomComboPreset.WhiteMageAfflatusFeature))
-                {
-                    if (level >= WHM.Levels.AfflatusSolace && gauge.Lily > 0)
-                        return WHM.AfflatusSolace;
-                }
-            }
-
-            return actionID;
-        }
-    }
 
     internal class WhiteMageMedica : CustomCombo
     {

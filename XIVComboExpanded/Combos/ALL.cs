5b05311262f64579d08d494e9aa6015c3921f192
Rubylium <alexisoko0@gmail.com>
2022-01-27 15:07:02 +0100

    Update ALL.cs

--- XIVComboExpanded/Combos/ALL.cs
+++ XIVComboExpanded/Combos/ALL.cs
@@ -7,6 +7,11 @@
 
         public const uint
             HeadGraze = 7551,
+            FootGraze = 7553,
+            LegGraze = 7554,
+            Feint = 7549,
+            Bloodbath = 7542,
+            LucidDream = 7562,
             SecondWind = 7541;
 
         public static class Buffs
@@ -18,13 +23,20 @@
         public static class Debuffs
         {
             public const ushort
+                Heavy = 14,
+                Bind = 13,
                 Placeholder = 0;
         }
 
         public static class Levels
         {
             public const byte
+                Feint = 22,
                 HeadGraze = 24,
+                FootGraze = 10,
+                LegGraze = 6,
+                Bloodbath = 12,
+                LucidDream = 24,
                 SecondWind = 8;
         }
     }
